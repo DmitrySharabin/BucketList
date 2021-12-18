df0635ad6021425099125b7fe7fbab15b89cfefe
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-12-18 13:56:29 +0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Dmitry Sharabin on 18.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
