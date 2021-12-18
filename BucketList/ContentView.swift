0907fb874c404627a16dbe41b90b9d1fa91a483e
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-12-18 14:36:26 +0300

    Day 68 — Adding conformance to Comparable for custom types

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,10 +7,36 @@
 
 import SwiftUI
 
+struct User: Identifiable, Comparable {
+    let id = UUID()
+    let firstName: String
+    let lastName: String
+    
+    static func <(lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
+}
+
 struct ContentView: View {
+//    let values = [1, 5, 3, 6, 2, 9].sorted()
+    
+    let users = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Kristine", lastName: "Kochanski"),
+        User(firstName: "David", lastName: "Lister")
+    ].sorted()
+//    .sorted {
+//        $0.lastName < $1.lastName
+//    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        List(values, id: \.self) {
+//            Text(String($0))
+//        }
+        
+        List(users) { user in
+            Text("\(user.lastName), \(user.firstName)")
+        }
     }
 }
 
